07bedbb3163a4d5efdffebf309cd17e4253df8be
AshuMishra <ashu.mishra007@yahoo.co.in>
2016-01-09 18:31:01 +0530

    Fixed positions of buttons

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var boardView: UIView!
+	@IBOutlet weak var button0: UIButton!
+	@IBOutlet weak var button1: UIButton!
+	@IBOutlet weak var button2: UIButton!
+	@IBOutlet weak var button3: UIButton!
+	@IBOutlet weak var button4: UIButton!
+	@IBOutlet weak var button5: UIButton!
+	@IBOutlet weak var button6: UIButton!
+	@IBOutlet weak var button7: UIButton!
+	@IBOutlet weak var button8: UIButton!
+
+	@IBOutlet weak var label: UILabel!
+	@IBOutlet weak var PlayerTurnsLabel: UILabel!
+	@IBOutlet weak var playAgainButton: UIButton!
+
+
+	var goNumber = 1
+	var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+	var winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+	var winner = 0
+
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
